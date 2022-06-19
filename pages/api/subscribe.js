842f039bf9c86c20850b7ab2beffdf005dedbf03
enicio0 <jessandro42@gmail.com>
2022-06-19 00:48:14 -0300

    Troca pelo pacote emailjs

--- pages/api/subscribe.js
+++ pages/api/subscribe.js
@@ -1,5 +1,6 @@
 import { PrismaClient } from '@prisma/client';
-import nodemailer from 'nodemailer';
+// import nodemailer from 'nodemailer';
+import { SMTPClient } from 'emailjs';
 
 const prisma = new PrismaClient();
 
@@ -15,6 +16,14 @@ export default async function handler(req, res) {
 
 async function createInquiry(req, res) {
     const body = req.body;
+
+    const client = new SMTPClient({
+        user: process.env.USER_MAIL,
+        password: process.env.PASSWORD_MAIL,
+        host: 'smtp.gmail.com',
+        ssl:true
+      });
+
     try {
         const newEntry = await prisma.users.create({
             data: {
@@ -22,27 +31,41 @@ async function createInquiry(req, res) {
                 email: body.email,
             }
         });
-        const transporter = nodemailer.createTransport({
-            port: 587,
-            host: process.env.SMTP_SERVER,
-            auth: {
-              user: process.env.USER_MAIL,
-              pass: process.env.PASSWORD_MAIL,
-            },
-        });
-        const mailData = {
-            from: 'jessandro42@gmail.com',
-            to: body.email,
-            subject: `Obrigado por assinar a minha Newsletter`,
-            text: 'Compre uma kombi',
-            html: '<div>Espero enviar algum conteudo toda terça!</div>'
-           }
-           transporter.sendMail(mailData, function (err, info) {
-            if(err)
-              console.log(err)
-            else
-              console.log(info)
-          })
+
+        client.send(
+            {
+                from: 'jessandro42@gmail.com',
+                to: body.email,
+                subject: `Obrigado por assinar a minha Newsletter`,
+                text: `Espero enviar algum conteudo toda terça!`,
+                html: '<div>Espero enviar algum conteudo toda terça!</div>'
+            }
+            )
+
+
+
+        // const transporter = nodemailer.createTransport({
+        //     port: 587,
+        //     host: process.env.SMTP_SERVER,
+        //     auth: {
+        //       user: process.env.USER_MAIL,
+        //       pass: process.env.PASSWORD_MAIL,
+        //     },
+        //     ssl:true
+        // });
+        // const mailData = {
+        //     from: 'jessandro42@gmail.com',
+        //     to: body.email,
+        //     subject: `Obrigado por assinar a minha Newsletter`,
+        //     text: 'Compre uma kombi',
+        //     html: '<div>Espero enviar algum conteudo toda terça!</div>'
+        //    }
+        //   transporter.sendMail(mailData, function (err, info) {
+        //     if(err)
+        //       console.log(err)
+        //     else
+        //       console.log(info)
+        //   })
 
         return res.status(200).json(newEntry, {success: true});
     } catch (error) {
